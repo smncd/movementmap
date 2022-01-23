1e815c8235c31825a6b5600c90b4eb0490b9f19a
twentyonepenguins <me@twentyonepenguins.xyz>
2022-01-23 21:10:49 +0100

    Spacing

--- src/lib/components/Marker.ts
+++ src/lib/components/Marker.ts
@@ -31,12 +31,15 @@ export function isMarkerData(
 ): object is MarkerOptions {
   const hasProperties = (keys: Array<unknown>) => {
     let hasProperties = true;
+
     keys.forEach((key) => {
       if (!Object.prototype.hasOwnProperty.call(object, key))
         hasProperties = false;
     });
+
     return hasProperties;
   };
+
   return hasProperties(['id', 'title', 'location']);
 }
 
