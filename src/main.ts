0a2e62330e4d44032215a95f24dc75438b3c0aac
twentyonepenguins <me@twentyonepenguins.xyz>
2022-01-23 19:13:47 +0100

    Basic map function

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,17 @@
-console.log('Hello World')
+import * as L from 'leaflet';
+import './styles/main.scss';
+
+function map() {
+  const map = L.map('map', {
+    center: [37.3, 4.57],
+    zoom: 3,
+  });
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+}
+
+map();
