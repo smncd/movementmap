895b824cc4f9a54bdeb5e255f30c524517bee9d1
twentyonepenguins <me@twentyonepenguins.xyz>
2022-01-23 20:50:00 +0100

    Created Marker class

---
+++ src/lib/components/Marker.ts
@@ -0,0 +1,46 @@
+import * as L from 'leaflet';
+import IconSVG from '../../images/icon.png';
+
+export interface MarkerOptions {
+  id: number | string;
+  title: string;
+  description?: string | HTMLElement;
+  location: {
+    address?: string;
+    day?: string;
+    time?: {
+      start?: string;
+      end?: string;
+    };
+    coordinates: {
+      lat: number;
+      lon: number;
+    };
+  };
+  contact?: {
+    email?: string;
+    website?: string;
+    instagram?: string;
+    facebook?: string;
+    twitter?: string;
+  };
+}
+
+export class Marker extends L.Marker {
+  constructor(markerData: MarkerOptions, markerIcon?: string) {
+    const { lat, lon } = markerData.location.coordinates;
+
+    const latlon: L.LatLngExpression = [lat, lon];
+
+    const icon = L.icon({
+      iconUrl: markerIcon ? markerIcon : IconSVG,
+      iconSize: [36, 55],
+      iconAnchor: [18, 54],
+      popupAnchor: [0, -35],
+    });
+
+    super(latlon, { icon });
+
+    this.bindPopup(`<h1>${markerData.title}</h1>`);
+  }
+}
