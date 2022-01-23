6923595594830304277f0673b341b75e7e29db3b
twentyonepenguins <me@twentyonepenguins.xyz>
2022-01-23 19:20:47 +0100

    MovementmMap class

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,10 @@
-import * as L from 'leaflet';
+import { MovementMap } from './lib/MovementMap';
 import './styles/main.scss';
 
-function map() {
-  const map = L.map('map', {
-    center: [37.3, 4.57],
-    zoom: 3,
-  });
-
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  }).addTo(map);
+declare global {
+  interface Window {
+    MovementMap?: any;
+  }
 }
 
-map();
+window.MovementMap = MovementMap;
