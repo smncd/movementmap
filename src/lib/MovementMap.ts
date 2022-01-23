5ff1b6a13d839a28e1c7d9dd62df7ee2843f9e14
twentyonepenguins <me@twentyonepenguins.xyz>
2022-01-23 19:31:52 +0100

    Default map options

--- src/lib/MovementMap.ts
+++ src/lib/MovementMap.ts
@@ -13,7 +13,25 @@ export interface MovementMapOptions extends L.MapOptions {
 }
 
 export class MovementMap {
-  constructor() {
+  private options: any = {
+    center: [37.3, 4.57],
+    zoom: 3,
+    minZoom: 1,
+    zoomControl: false,
+    attributionControl: false,
+    controls: {
+      zoom: true,
+      attribution: true,
+    },
+    tiles: {
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      options: {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      },
+    },
+  };
     const map = L.map('map', {
       center: [37.3, 4.57],
       zoom: 3,
