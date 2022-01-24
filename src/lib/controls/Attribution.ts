1366789c2631a4fdc460f823243b00332a6487fc
twentyonepenguins <me@twentyonepenguins.xyz>
2022-01-24 03:14:45 +0100

    Returning empty div

--- src/lib/controls/Attribution.ts
+++ src/lib/controls/Attribution.ts
@@ -1,10 +1,27 @@
 import * as L from 'leaflet';
 
 export class AttributionControl extends L.Control {
+  private map: L.Map = null;
+  private attribution: string = null;
+  private controlName = 'leaflet-control-attribution';
+  private container: HTMLElement = L.DomUtil.create(
+    'div',
+    `${this.controlName} leaflet-bar`
+  );
+
   constructor(
     attribution: string,
     options: L.ControlOptions = { position: 'bottomright' }
   ) {
     super(options);
+
+    this.attribution = attribution;
+  }
+
+  onAdd(map: L.Map): HTMLElement {
+    this.map = map;
+    const container = this.container;
+
+    return container;
   }
 }
