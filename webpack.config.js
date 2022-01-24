1411b0e457e5a9d9a16024dbe40169186e3fa2dd
twentyonepenguins <me@twentyonepenguins.xyz>
2022-01-24 01:28:43 +0100

    Default to production

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
-  mode: process.env.NODE_ENV || 'development',
+  mode: process.env.NODE_ENV || 'production',
   entry: './src/main.ts',
   plugins: [
     new MiniCssExtractPlugin({
