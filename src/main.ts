f6ae0b86c7d17311676cc5ec8ce10db691715dac
twentyonepenguins <me@twentyonepenguins.xyz>
2022-01-24 02:23:56 +0100

    Updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
+/* @preserve
+ * MovementMap 0.1.0
+ *
+ * A interactive map for the climate movement based on Leaflet (https://leafletjs.com)
+ */
+
 import { MovementMap } from './lib/MovementMap';
 import './styles/main.scss';
 
