08ea13d8bb499f6d0fac528dfa4fc25ad46abd7d
twentyonepenguins <me@twentyonepenguins.xyz>
2022-01-30 18:35:16 +0100

    Language interface

---
+++ src/languages/index.ts
@@ -0,0 +1,23 @@
+import languages from './languages.json';
+
+export interface Language {
+  language: string;
+  groups: {
+    zoom: {
+      in: string;
+      out: string;
+      reset: string;
+    };
+    attribution: {
+      title: string;
+    };
+    popup: {
+      address: string;
+      time: string;
+      description: string;
+      contact: string;
+      email: string;
+      website: string;
+    };
+  };
+}
