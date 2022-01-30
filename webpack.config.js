964486a1914224a7196dae621888ba0634f63f1b
twentyonepenguins <me@twentyonepenguins.xyz>
2022-01-30 22:16:20 +0100

    Enable source maps in development

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'production',
+  devtool: process.env.NODE_ENV === 'development' && 'eval-source-map',
   entry: './main.ts',
   plugins: [
     new MiniCssExtractPlugin({
