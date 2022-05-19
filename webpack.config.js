50cbd26d6e84448d505519f148e9a99be6ffcc3a
twentyonepenguins <contact@twentyonepenguins.xyz>
2022-05-19 23:51:46 +0200

    Moved main.ts to src

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 module.exports = {
   mode: process.env.NODE_ENV || 'production',
   devtool: process.env.NODE_ENV === 'development' && 'eval-source-map',
-  entry: './main.ts',
+  entry: './src/main.ts',
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'movementmap.css',
